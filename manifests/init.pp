9975785554465d245d3a19134dc93b0b344e946b
Adam Breaux <breauxaj@gmail.com>
2015-09-03 16:04:40 -0400

    manage file/folder perms

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,4 +7,20 @@ class yum (
 
   package { $required: ensure => $ensure }
 
+  file { '/etc/yum.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[$required],
+  }
+
+  file { '/etc/yum.repos.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755'
+    require => Package[$required],
+  }
+
 }
